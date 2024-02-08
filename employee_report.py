c8153bb07a4a16ae1b72d1a56c6ba0b7bdb785d7
Garrett <Garrett_austin2@baylor.edu>
2024-02-08 13:50:50 -0600

    Finishing touches on employee report

--- employee_report.py
+++ employee_report.py
@@ -6,8 +6,8 @@
 ## Management would also like to know who and how many are in different age categories - 
 ## 20s, 30s and 40s. Reproduce the report as show below (print statements).
 import csv
-ages = {'20s': [], '30s': [], '40s': []} #Establishes dictionary for age with keys
-categories = {'Highly Efficient': [], 'Inefficient': []} # Establishes dictionary for efficiency with keys
+ages = {'20s': [], '30s': [], '40s': []} #Establishes dictionary for age with keys but no records
+categories = {'Highly Efficient': [], 'Low Efficiency': []} # Establishes dictionary for efficiency with keys but no records
 
 employee = open('employee_data.csv','r')
 csv_file = csv.reader(employee)
@@ -18,27 +18,28 @@ for rec in csv_file:    #This brings each record into a loop
     prod = int(rec[5])
     hrs = int(rec[4])
     efficiency = (prod / hrs)
-#efficiency calculations     
+#efficiency   
     if efficiency >= 2:
-        categories['Highly Efficient'].append(name) # appends name to efficiency dictionary list
+        categories['Highly Efficient'].append(name) # appends name to end of the efficiency dictionary list
     elif efficiency <= 1:
-        categories['Inefficient'].append(name)
-#age calculation
-    if 20 <= age < 30:
-        ages['20s'].append(name) # appends name to age dictionary list
-    elif 30 <= age < 40:
+        categories['Low Efficiency'].append(name)
+#age
+    if age < 30 and age >= 20:
+        ages['20s'].append(name) # appends name to the end of the age dictionary list
+    elif age < 40 and age >= 30:
         ages['30s'].append(name) 
-    elif 40 <= age < 50:
+    elif age < 50 and age >= 40:
         ages['40s'].append(name)
     #input()
 print("Efficiency Categories:")
 for category, names in categories.items():
-    print(f"\nEmployees who are {category}:")
-    for name in names:
+    print(f"\n{category} Individuals:")
+    for name in names: # Starts loop to print names from dictionary
         print(f"{name}")
+        
 print("\nAge Groups:")
 for agegroup, names in ages.items():
     print(f"\nEmployees in their {agegroup}:")
-    for name in names:
+    for name in names: # Starts loop to print names from dictionary
         print(f"{name}")
     print(f"Total number of employees in their {agegroup}: {len(names)}")
